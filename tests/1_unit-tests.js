1168f11f31ff8e166136889a6f0907b36b4260bb
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-03-05 20:36:13 +0700

    add tests barebone

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,5 +4,100 @@ const assert = chai.assert;
 const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
+  test("Translate Mangoes are my favorite fruit. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate I ate yogurt for breakfast. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate We had a party at my friend's condo. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate Can you toss this in the trashcan for me? to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate The parking lot was full. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate Like a high tech Rube Goldberg machine. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate To play hooky means to skip class or work. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate No Mr. Bond, I expect you to die. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate Dr. Grosh will see you now. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate Lunch is at 12:15 today. to British English", function () {
+    assert.equal();
+  });
+
+  test("Translate We watched the footie match for a while. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate Paracetamol takes up to an hour to work. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate First, caramelise the onions. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate I spent the bank holiday at the funfair. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate I had a bicky then went to the chippy. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate I've just got bits and bobs in my bum bag. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate The car boot sale at Boxted Airfield was called off. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate Have you met Mrs Kalyani? to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate Prof Joyner of King's College, London. to American English", function () {
+    assert.equal();
+  });
+
+  test("Translate Tea time is usually around 4 or 4.30. to American English", function () {
+    assert.equal();
+  });
+
+  test("Highlight translation in Mangoes are my favorite fruit.", function () {
+    assert.equal();
+  });
+
+  test("Highlight translation in I ate yogurt for breakfast.", function () {
+    assert.equal();
+  });
+
+  test("Highlight translation in We watched the footie match for a while.", function () {
+    assert.equal();
+  });
+
+  test("Highlight translation in Paracetamol takes up to an hour to work.", function () {
+    assert.equal();
+  });
 
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,82 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
+  test("Translation with text and locale fields: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post(baseUrl)
+      .send({})
+      .end(function (err, res) {
+        if (err) assert.fail(err);
+        assert.equal(res.status, 200, "Server should return a 200");
+        done();
+      });
+  });
 
+  test("Translation with text and invalid locale field: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post(baseUrl)
+      .send({})
+      .end(function (err, res) {
+        if (err) assert.fail(err);
+        assert.equal(res.status, 200, "Server should return a 200");
+        done();
+      });
+  });
+
+  test("Translation with missing text field: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post(baseUrl)
+      .send({})
+      .end(function (err, res) {
+        if (err) assert.fail(err);
+        assert.equal(res.status, 200, "Server should return a 200");
+        done();
+      });
+  });
+
+  test("Translation with missing locale field: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post(baseUrl)
+      .send({})
+      .end(function (err, res) {
+        if (err) assert.fail(err);
+        assert.equal(res.status, 200, "Server should return a 200");
+        done();
+      });
+  });
+
+  test("Translation with empty text: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post(baseUrl)
+      .send({})
+      .end(function (err, res) {
+        if (err) assert.fail(err);
+        assert.equal(res.status, 200, "Server should return a 200");
+        done();
+      });
+  });
+
+  test("Translation with text that needs no translation: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post(baseUrl)
+      .send({})
+      .end(function (err, res) {
+        if (err) assert.fail(err);
+        assert.equal(res.status, 200, "Server should return a 200");
+        done();
+      });
+  });
 });
