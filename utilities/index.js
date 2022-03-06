1c8f2873464131df8c8b6baa87c1541fc1eb8e03
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-03-06 18:11:35 +0700

    add new derivative error class

--- utilities/index.js
+++ utilities/index.js
@@ -2,8 +2,10 @@
 
 const swapKeyValue = require("./swap-key-value");
 const titleCase = require("./title-case");
+const ErrorStatus = require("./error-status");
 
 module.exports = {
   swapKeyValue,
   titleCase,
+  ErrorStatus,
 };
